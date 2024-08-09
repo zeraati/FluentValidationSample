19ae2137a48bff4c4949b208479f4069d6fa8f53
zeraati <zeraatimail@gmail.com>
2024-08-09 16:44:27 +0330

    Add project files.

---
+++ FluentValidationSample/Controllers/ValuesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FluentValidationSample.Controllers;
+[Route("api/[controller]"), ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        
+
+        return Ok();
+    }
+}
